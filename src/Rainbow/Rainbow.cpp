c17857c00e75e324dae06975617b5850f169d038
timm <timm@tempmachine>
2024-04-15 20:26:58 +0200

    lmaocat wont print a line more

--- src/Rainbow/Rainbow.cpp
+++ src/Rainbow/Rainbow.cpp
@@ -1,30 +1,33 @@
 #include "Rainbow.hpp"
 
-#include <random>
-#include <cstdio>
 #include <chrono>
+#include <climits>
+#include <csignal>
+#include <cstddef>
+#include <cstdint>
+#include <cstdio>
+#include <cwchar>
+#include <istream>
+#include <random>
 #include <string>
 #include <thread>
-#include <csignal>
-#include <climits>
-#include <cstddef>
-#include <sstream>
 
 #include <unistd.h>
 
-#include "Args/Args.hpp"
 #include "ANSI/ANSI.hpp"
+#include "Args/Args.hpp"
 
 
+#define BASE_SPEED 1000.0
 #define TAB 8
 
 
 Rainbow::Rainbow(const Args &settings):
-            frequency(settings.frequency),
-            spread   (settings.spread),
-            duration (settings.duration),
-            animate  (settings.animate),
-            sleep_time(static_cast<int>((1000.0 / settings.speed))) {
+    frequency(settings.frequency),
+    spread(settings.spread),
+    duration(settings.duration),
+    animate(settings.animate),
+    sleep_time(static_cast<int>((BASE_SPEED / settings.speed))) {
 
     bool truecolor = settings.truecolor;
     if (!settings.truecolor) {
@@ -33,16 +36,12 @@ Rainbow::Rainbow(const Args &settings):
             mode = std::getenv("COLORTERM");
         }
 
-        if (mode == "truecolor" || mode == "24bit") {
-            truecolor = true;
-        } else {
-            truecolor = false;
-        }
+        truecolor = (mode == "truecolor" || mode == "24bit");
     }
 
     // https://stackoverflow.com/a/59511764
     // 1 = std::cout -> STDOUT_FILENO
-    this->truecolor_enabled = (settings.force || (isatty(STDOUT_FILENO) && truecolor));
+    this->truecolor_enabled = (settings.force || (isatty(STDOUT_FILENO) == 1 && truecolor));
 
     if (settings.seed == 0) {
         std::random_device r_device;
@@ -53,7 +52,7 @@ Rainbow::Rainbow(const Args &settings):
         this->state = static_cast<double>(settings.seed);
     }
 
-    this->ansi_handler = ANSI{settings.invert};
+    this->ansi_handler = ANSI{ settings.invert };
 
     if (this->truecolor_enabled) {
         Signal_Handler::setup();
@@ -61,56 +60,67 @@ Rainbow::Rainbow(const Args &settings):
 }
 
 
-void Rainbow::cat(std::wistream &is) {
+void Rainbow::cat(std::wistream &input) {
     if (this->truecolor_enabled) {
         if (this->animate) {
             ANSI::hide_cursor();
         }
 
+        bool chomped{};
         auto ansi_state = State::NONE;
-        for (std::wstring line; std::getline(is, line);) {
+        for (std::wstring line; getline(input, line, chomped);) {
             if (this->animate) {
                 print_animated(line, ansi_state);
-            }
-            else {
+            } else {
                 print_plain(line, ansi_state);
             }
 
+            if (chomped) {
+                std::putwchar(L'\n');
+            }
+
             this->state += this->spread;
-            std::putwchar(L'\n');
         }
 
         ANSI::reset_ansi_in_terminal();
     }
     else {
-        for (std::wstring line; std::getline(is, line);) {
-            std::wprintf(L"%ls\n", line.c_str());
+        bool chomped{};
+        for (std::wstring line; getline(input, line, chomped);) {
+            std::fputws(line.c_str(), stdout);
+
+            if (chomped) {
+                std::putwchar(L'\n');
+            }
         }
     }
 }
 
 
 void Rainbow::print_plain(const std::wstring &line, State &ansi_state) const {
-    std::size_t i = 0;
+    std::size_t index = 0;
 
     if (ANSI::incomplete_ansi(ansi_state)) {
-        ANSI::read_incomplete(line, i, ansi_state);
+        ANSI::read_incomplete(line, index, ansi_state);
     }
 
     std::size_t counter = 0;
-    for (; i < line.length(); ++i) {
-        if (ANSI::is_esc(line[i])) {
-            ANSI::read(line, i, ansi_state);
-            --i;
+    RGB color;
+    for (; index < line.length(); ++index) {
+        if (ANSI::is_esc(line[index])) {
+            ANSI::read(line, index, ansi_state);
+            --index;
         }
-        else if (line[i] == '\t') {
+        else if (line[index] == '\t') {
             for (int j = 0; j < TAB; ++j) {
-                this->ansi_handler.paint(rainbow(counter), L' ');
+                rainbow(counter, color);
+                this->ansi_handler.paint(color, L' ');
                 ++counter;
             }
         }
         else {
-            this->ansi_handler.paint(rainbow(counter), line[i]);
+            rainbow(counter, color);
+            this->ansi_handler.paint(color, line[index]);
             ++counter;
         }
     }
@@ -120,10 +130,10 @@ void Rainbow::print_plain(const std::wstring &line, State &ansi_state) const {
 void Rainbow::print_animated(const std::wstring &line, State &ansi_state) {
     ANSI::save_cursor_position();
 
-    const double state_backup = this->state;
+    const double state_backup          = this->state;
     const State state_before_line_read = ansi_state;
 
-    for (long i = 0; i < this->duration; ++i) {
+    for (std::int64_t i = 0; i < this->duration; ++i) {
         ANSI::restore_cursor_position();
         ansi_state = state_before_line_read;
         this->state += this->spread;
@@ -135,12 +145,12 @@ void Rainbow::print_animated(const std::wstring &line, State &ansi_state) {
 }
 
 
-RGB Rainbow::rainbow(const std::size_t index) const {
-    auto i = static_cast<double>(index);
+void Rainbow::rainbow(const std::size_t index, RGB &color) const {
+    auto index_f = static_cast<double>(index);
 
-    i += this->state;
-    i /= this->spread;
-    i *= this->frequency;
+    index_f += this->state;
+    index_f /= this->spread;
+    index_f *= this->frequency;
 
     constexpr double PI = 3.14159;
 
@@ -148,15 +158,35 @@ RGB Rainbow::rainbow(const std::size_t index) const {
     constexpr double GREEN = 2.0 * PI / 3.0;
     constexpr double BLUE  = 4.0 * PI / 3.0;
 
-    const double red_f   = std::sin(i + RED)   * 127.0 + 128.0;
-    const double green_f = std::sin(i + GREEN) * 127.0 + 128.0;
-    const double blue_f  = std::sin(i + BLUE)  * 127.0 + 128.0;
+    const double red_f   = std::sin(index_f + RED)   * 127.0 + 128.0;
+    const double green_f = std::sin(index_f + GREEN) * 127.0 + 128.0;
+    const double blue_f  = std::sin(index_f + BLUE)  * 127.0 + 128.0;
 
-    const auto red   = static_cast<int>(red_f);
-    const auto green = static_cast<int>(green_f);
-    const auto blue  = static_cast<int>(blue_f);
+    color.red   = static_cast<int>(red_f);
+    color.green = static_cast<int>(green_f);
+    color.blue  = static_cast<int>(blue_f);
+}
 
-    return RGB{red, green, blue};
+
+auto Rainbow::getline(std::wistream &input, std::wstring &line, bool &chomped) -> std::wistream& {
+    line.clear();
+    chomped = false;
+
+    for (wchar_t character{}; input.get(character);) {
+        if (character != L'\n') {
+            line += character;
+        }
+        else {
+            chomped = true;
+            break;
+        }
+    }
+
+    if (input.eof() && !line.empty()) {
+        input.clear();
+    }
+
+    return input;
 }
 
 
@@ -175,5 +205,7 @@ void Rainbow::Signal_Handler::setup() {
 
 void Rainbow::Signal_Handler::handler(const int signal) {
     ANSI::reset_ansi_in_terminal();
-    std::exit(128 + signal);
+
+    const int exit_code = 128 + signal;
+    std::exit(exit_code);
 }
--- src/Rainbow/Rainbow.hpp
+++ src/Rainbow/Rainbow.hpp
@@ -1,41 +1,43 @@
 #ifndef RAINBOW_HPP
 #define RAINBOW_HPP
 
-
 #include <chrono>
-#include <string>
 #include <cstddef>
-#include <sstream>
+#include <cstdint>
+#include <cwchar>
+#include <istream>
+#include <string>
 
-#include "Args/Args.hpp"
 #include "ANSI/ANSI.hpp"
+#include "Args/Args.hpp"
 
 
 class Rainbow {
 public:
-    explicit Rainbow(const Args &settings);
+    explicit Rainbow(const Args& settings);
 
-    void cat(std::wistream &is);
+    void cat(std::wistream& input);
 
 private:
-    void print_plain(const std::wstring &line, State &ansi_state) const;
-    void print_animated(const std::wstring &line, State &ansi_state);
+    void print_plain(const std::wstring& line, State& ansi_state) const;
+    void print_animated(const std::wstring& line, State& ansi_state);
 
-    [[nodiscard]] RGB rainbow(std::size_t index) const;
+    void rainbow(std::size_t index, RGB& color) const;
+
+    static auto getline(std::wistream& input, std::wstring& line, bool& chomped) -> std::wistream&;
 
     double state{};
 
-    ANSI ansi_handler{};
+    ANSI ansi_handler;
 
     bool truecolor_enabled{};
 
     const double frequency;
     const double spread;
 
-    const long duration;
+    const std::int64_t duration;
     const bool animate;
     const std::chrono::milliseconds sleep_time;
-
 
     struct Signal_Handler {
         static void setup();
